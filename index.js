efe67a2d44e5fcbdaa63c6228dcad49c1604bf04
Loc.Tong <tbloc@outlook.com>
2022-10-30 16:04:56 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+// module.exports = async function (context, req) {
+//     context.log('JavaScript HTTP trigger function processed a request.');
+
+//     const name = (req.query.name || (req.body && req.body.name));
+//     const responseMessage = name
+//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//         : "This HTTP triggered function executed successfully. Pass a name on the query string or in the request body for a personalized response.";
+
+//     context.res = {
+//         // status: 200, /* Defaults to 200 */
+//         body: responseMessage
+//     };
+// }
+
+module.exports = function (context, req) {
+    context.log('Drive Gear Temperature Service triggered');
+    if (req.body && req.body.readings) {
+        req.body.readings.forEach(function(reading) {
+
+            if(reading.temperature<=25) {
+                reading.status = 'OK';
+            } else if (reading.temperature<=50) {
+                reading.status = 'CAUTION';
+            } else {
+                reading.status = 'DANGER'
+            }
+            context.log('Reading is ' + reading.status);
+        });
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {
+                "readings": req.body.readings
+            }
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please send an array of readings in the request body"
+        };
+    }
+    context.done();
+};
